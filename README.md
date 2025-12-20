acdb8e393ae16837d9aa1adaa03fd4fd6691b2ec
Alina Boiarkina <alinaboyarkina.ua@gmail.com>
2025-12-20 20:27:35 +0100

    Fix_1

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# alinabo_test_repo
-My first test repository
+# Playwright Tests – Practice Software Testing
+
+## Technologies
+
+* JavaScript
+* TypeScript
+* Node.js
+* Playwright
+
+
+## Installation
+```bash
+npm install
+npx playwright install
+```
+
+## Run tests
+```bash
+npx playwright test
+```
+
+Run tests with browser UI:
+
+```bash
+npx playwright test --headed
+```
+
+## Test example
+The project includes a **login test** that:
+
+* opens login page
+* enters email and password
+* submits the form
+* verifies successful login
+
+Test credentials:
+
+```
+Email: customer@practicesoftwaretesting.com
+Password: welcome01
+```
+
+## License
+This project is for educational purposes.
+
+## Author
+Alina Boyarkina
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,11 +1,18 @@
 import { test, expect } from '@playwright/test'
 
 test ('Validate login successful', async ({page}) => {
-  await page.goto('https://practicesoftwaretesting.com/auth/login');
-  await page.locator('[data-test="email"]').fill('customer@practicesoftwaretesting.com');
-  await page.locator('[data-test="password"]').fill('welcome01');
-  await page.locator('[data-test="login-submit"]').click();
-  await expect (page).toHaveURL('https://practicesoftwaretesting.com/account');
-  await expect (page).toHaveTitle('Overview - Practice Software Testing - Toolshop - v5.0');
-  await expect(page.getByText('Jane Doe')).toBeVisible();
+
+    await page.goto('https://practicesoftwaretesting.com/auth/login');
+    await page.locator('[data-test="email"]').fill('customer@practicesoftwaretesting.com');
+    await page.locator('[data-test="password"]').fill('welcome01');
+    await page.locator('[data-test="login-submit"]').click();
+    
+    await expect (page).toHaveURL('https://practicesoftwaretesting.com/account');
+    
+    await page.waitForSelector('h1:has-text("My account")');
+    
+    await expect(page).toHaveTitle('Overview - Practice Software Testing - Toolshop - v5.0');
+    
+    await expect(page.getByText('Jane Doe')).toBeVisible();
+
 });
