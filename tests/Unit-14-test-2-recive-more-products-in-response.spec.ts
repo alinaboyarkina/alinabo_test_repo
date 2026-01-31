47f3d33fa6ec3b266d4cfbd429dea115da80d780
Alina Boiarkina <alinaboyarkina.ua@gmail.com>
2026-01-31 09:37:20 +0100

    Unit-14 Fix_6

--- tests/Unit-14-test-2-recive-more-products-in-response.spec.ts
+++ tests/Unit-14-test-2-recive-more-products-in-response.spec.ts
@@ -4,19 +4,25 @@ import { mockProductsRoute } from '../mocks/products.route';
 
 test('`Verify 20 products are displayed per page', async ({ apiLoggedInApp }) => {
 
-    const expectedCount = 20;
+    const expectedCount = 21;
     
     await mockProductsRoute(apiLoggedInApp.page, expectedCount);
     await apiLoggedInApp.homePage.open();
 
-    const products = apiLoggedInApp.homePage.productCard;
+    //const products = apiLoggedInApp.homePage.productCard;
 
-    await expect(apiLoggedInApp.homePage.productCard.first()).toContainText('Mock product');
+    //await expect(apiLoggedInApp.homePage.productCard.first()).toContainText('Mock product');
 
-    await expect(
-        products,
-        'Number of products displayed is incorrect',
-    ).toHaveCount(expectedCount);
+    // await expect(
+    //     products,
+    //     'Amount of products displayed is incorrect',
+    // ).toHaveCount(expectedCount);
+
+    await expect(apiLoggedInApp.homePage.productCard).toHaveCount(expectedCount)
+
+    for (let i = 0; i < expectedCount; i++) { 
+        await expect(apiLoggedInApp.homePage.productCard.nth(i)).toBeVisible(); 
+    }
 });
 
 
