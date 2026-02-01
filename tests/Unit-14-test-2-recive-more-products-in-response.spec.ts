bb1ed5c442624753d8a659ab31d79f559d52a70e
Alina Boiarkina <alinaboyarkina.ua@gmail.com>
2026-02-01 11:56:08 +0100

    Unit-14 Fix_14

--- tests/Unit-14-test-2-recive-more-products-in-response.spec.ts
+++ tests/Unit-14-test-2-recive-more-products-in-response.spec.ts
@@ -7,7 +7,7 @@ test('`Verify 20 products are displayed per page', async ({ apiLoggedInApp }) =>
     const expectedCount = 20;
     
     await mockProductsRoute(apiLoggedInApp.page, expectedCount);
-    await apiLoggedInApp.page.reload();
+    await apiLoggedInApp.page.goto('/');
     await apiLoggedInApp.page.waitForResponse('**/products**');
     
     const expectProducts = apiLoggedInApp.homePage.productCard;
