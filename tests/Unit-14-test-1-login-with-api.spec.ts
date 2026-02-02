f4f0ee8055e9ae8846599d6c2c34cf09258601e6
Alina Boiarkina <alinaboyarkina.ua@gmail.com>
2026-02-02 15:09:16 +0100

    Unit-14 Fix_18

--- tests/Unit-14-test-1-login-with-api.spec.ts
+++ tests/Unit-14-test-1-login-with-api.spec.ts
@@ -5,6 +5,7 @@ import { TEST_USER } from '../testData/testUser';
 
 test ('Validate login successful with API', async ({apiLoggedInApp}) => {
     
+    await apiLoggedInApp.homePage.open()
     await expect(apiLoggedInApp.homePage.header.navMenuButton).toHaveText(TEST_USER.fullName);
 
 });
