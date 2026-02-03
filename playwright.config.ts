069e9816d7c4d74f39f5b60bcac922af775269ab
Alina Boiarkina <alinaboyarkina.ua@gmail.com>
2026-02-03 16:19:41 +0100

    Unit-15 Fix_7

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,15 +23,31 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  //reporter: 'html',
+  reporter: [ 
+    ['dot'], 
+    ['html', { open: 'never' }],
+    ['json', { outputFile: 'report.json' }], 
+    ['list'],
+      [
+        '@testomatio/reporter/playwright',
+        {
+          apiKey: process.env.TESTOMATIO,
+        },
+      ],
+    ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
     baseURL: BASE_URL,
 
+    screenshot: 'only-on-failure',
+    video: 'on-first-retry',
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    //trace: 'on-first-retry',
+    trace: 'retain-on-failure',
 
     testIdAttribute: 'data-test',
   },
